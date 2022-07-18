e20f882b1517ed3664815921b2ff8cbf1c38399c
Thiago Passarin <thiagopas@gmail.com>
2022-07-18 16:56:30 -0400

    First working examples.

---
+++ t265_example.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+## License: Apache 2.0. See LICENSE file in root directory.
+## Copyright(c) 2019 Intel Corporation. All Rights Reserved.
+
+#####################################################
+##           librealsense T265 example             ##
+#####################################################
+
+# First import the library
+import pyrealsense2 as rs
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+
+def set_axes_equal(ax):
+    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc..  This is one possible solution to Matplotlib's
+    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+
+    Input
+      ax: a matplotlib axis, e.g., as output from plt.gca().
+    '''
+
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])
+    x_middle = np.mean(x_limits)
+    y_range = abs(y_limits[1] - y_limits[0])
+    y_middle = np.mean(y_limits)
+    z_range = abs(z_limits[1] - z_limits[0])
+    z_middle = np.mean(z_limits)
+
+    # The plot bounding box is a sphere in the sense of the infinity
+    # norm, hence I call half the max range the plot radius.
+    plot_radius = 0.5*max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+
+
+# Declare RealSense pipeline, encapsulating the actual device and sensors
+pipe = rs.pipeline()
+
+# Build config object and request pose data
+cfg = rs.config()
+cfg.enable_stream(rs.stream.pose)
+
+# Start streaming with requested config
+pipe.start(cfg)
+
+matplotlib.interactive(True)
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+xlist = list()
+ylist = list()
+zlist = list()
+
+try:
+    #for _ in range(50):
+    while True:
+        # Wait for the next set of frames from the camera
+        frames = pipe.wait_for_frames()
+
+        # Fetch pose frame
+        pose = frames.get_pose_frame()
+        if pose:
+            # Print some of the pose data to the terminal
+            data = pose.get_pose_data()
+            xlist.append(data.translation.x)
+            ylist.append(data.translation.y)
+            zlist.append(data.translation.z)
+            ax.cla()
+            ax.plot3D(xlist, ylist, zlist)
+            plt.draw()
+            print("Frame #{}".format(pose.frame_number))
+            set_axes_equal(ax)
+            plt.pause(.01)
+
+finally:
+    pipe.stop()
