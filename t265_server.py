2e94e9605dc7fbb72101a0fd2374e938a8af5fd0
Thiago Passarin <thiagopas@gmail.com>
2022-07-20 11:02:46 -0400

    Logging to static folder. TODO: - Start/stop logging without restarting server - Create an index of log files accessible via browser

--- t265_server.py
+++ t265_server.py
@@ -25,34 +25,6 @@ from datetime import datetime
 SAVE_LOG = True
 SHOW_PLOT_REALTIME = False
 
-def set_axes_equal(ax):
-    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
-    cubes as cubes, etc..  This is one possible solution to Matplotlib's
-    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
-
-    Input
-      ax: a matplotlib axis, e.g., as output from plt.gca().
-    '''
-
-    x_limits = ax.get_xlim3d()
-    y_limits = ax.get_ylim3d()
-    z_limits = ax.get_zlim3d()
-
-    x_range = abs(x_limits[1] - x_limits[0])
-    x_middle = np.mean(x_limits)
-    y_range = abs(y_limits[1] - y_limits[0])
-    y_middle = np.mean(y_limits)
-    z_range = abs(z_limits[1] - z_limits[0])
-    z_middle = np.mean(z_limits)
-
-    # The plot bounding box is a sphere in the sense of the infinity
-    # norm, hence I call half the max range the plot radius.
-    plot_radius = 0.5*max([x_range, y_range, z_range])
-
-    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
-    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
-    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
-
 # Create pose queue
 qpose_log = queue.Queue()
 qpose_plt = queue.Queue()
@@ -75,18 +47,9 @@ pipe.start(cfg)
 now = datetime.now()
 dt_string = now.strftime("log-%Y-%m-%d-%H-%M-%S")
 
-if SHOW_PLOT_REALTIME is True:
-    matplotlib.interactive(True)
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    xlist = list()
-    ylist = list()
-    zlist = list()
 
 if SAVE_LOG is True:
-    logfile = open(dt_string + '.txt', 'wt')
+    logfile = open('./static/' + dt_string + '.txt', 'wt')
 
 def producer(pipe, qpose_plt, qpose_log, evt_quit:threading.Event):
     while not evt_quit.is_set():
@@ -121,7 +84,6 @@ def producer(pipe, qpose_plt, qpose_log, evt_quit:threading.Event):
                       str(data.angular_velocity.z) + ',' + \
                       str(data.mapper_confidence) + '\n'
             qpose_plt.put(newline)
-            #qpose_plt.get()
             qpose_log.put(newline)
 
 @app.route('/log',methods=["GET","POST"])
